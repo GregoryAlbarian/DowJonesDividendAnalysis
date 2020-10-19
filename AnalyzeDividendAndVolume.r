e28549314536bf9a31ad4cc4ab6bfcc096145f54
GregoryAlbarian <greg1999@sbcglobal.net>
2020-10-19 14:34:42 -0700

    added everything up to volumes and graph rough draft completed

--- AnalyzeDividendAndVolume.r
+++ AnalyzeDividendAndVolume.r
@@ -1,4 +1,4 @@
-# line 2-4 I learned about on stack overflow
+# line 3 to 11 I learned about on stack overflow
 # it should upload ggplot2 if you don't have it already
 list.of.packages <- c("ggplot2")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -13,6 +13,8 @@ if(length(new.packages)) install.packages(new.packages)
 # using these libraries
 library(ggplot2)
 library(rvest)
+# this program also requires a stable internet
+# connection to work and scrape appropriate data
 
 # importing in my data
 
@@ -32,17 +34,35 @@ dividend_table$Yield <- substr(dividend_table$Yield,1,nchar(dividend_table$Yield
 dividend_table$Yield <- as.numeric(dividend_table$Yield)
 # yield is still stored as a percentage for later calculations
 
-# ticker symbols used
+# ticker symbols used maybe need
 ticker_symbols <- dividend_table[,1]
 
 # getting volumes for each stock
 
+# congjoined data will be filled up late in the for loop
+conjoined_data <- data.frame(symbols = c(), volumes = c(), dividends = c())
+
+iteration_amount <- seq(1:30)
+for (iteration in iteration_amount) {
+  file_names <- paste("stock_volumes/",ticker_symbols,".csv",sep = "")
+  stock <-read.csv(file_names[iteration])
+  volume <- stock[974:1224, 6]
+  average_volume <- mean(volume)
+  
+  temp_data <- data.frame(ticker_symbols[iteration], average_volume, dividend_table$Yield[iteration])
+  conjoined_data <- rbind(conjoined_data, temp_data)
+}
+
+colnames(conjoined_data) <- c("symbol", "average_volume", "dividends")
+# ratio look at the similarity between volume and dividend
+conjoined_data$ratio <- conjoined_data$average_volume / conjoined_data$dividends
+conjoined_data
 
 # graph
-
-
-#put in README take out of script
-# https://www.reinisfischer.com/dow-jones-companies-yield-2018
-# https://www.kaggle.com/timoboz/stock-data-dow-jones?select=AAPL.csv
-# https://www.tutorialspoint.com/r/r_data_frames.htm
-# https://r.789695.n4.nabble.com/remove-last-char-of-a-text-string-td2254377.html
+graph<-ggplot(conjoined_data, aes(x=conjoined_data$average_volume, y=conjoined_data$dividends))
+graph_title <- "The way the dividend effect the average stock in each company in the Dow Jones "
+graph_title <- paste(graph_title, "Industrial Average (DOW) for 2018")
+graph <- graph + labs(title = graph_title, x = "dividend (%)", y = "average volume")
+graph <- graph + geom_point(colour="blue")
+graph <- graph + geom_smooth(model="lm", color = "red")
+graph
